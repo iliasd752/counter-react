2c5a993d1bfde01c1b0eccafd556992624f4158e
Ilias <ilias.dinia@outlook.com>
2024-07-06 14:50:34 +0300

    Implementing event handlers

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,16 +1,3 @@
-import { useState } from "react";
-
-export default function Count() {
-  const [count, setCount] = useState(5);
-
-  return (
-    <p
-      onClick={() => {
-        setCount(count + 1);
-      }}
-      className="count"
-    >
-      {count}
-    </p>
-  );
+export default function Count({ count }) {
+  return <p className="count">{count}</p>;
 }
