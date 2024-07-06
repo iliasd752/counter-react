1658f3aed86263f830cb8cd6ef099b047cfaa9d7
Ilias <ilias.dinia@outlook.com>
2024-07-06 15:52:55 +0300

    Implementing conditional rendering

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,18 +1,26 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function CountButton({ type, setCount }) {
+export default function CountButton({ type, setCount, locked }) {
   const handleClick = () => {
     setCount((prev) => {
       if (type === "minus") {
-        return prev - 1;
+        const newCount = prev - 1;
+        if (newCount < 0) {
+          return 0;
+        }
+        return newCount;
       } else {
-        return prev + 1;
+        const newCount = prev + 1;
+        if (newCount > 5) {
+          return 5;
+        }
+        return newCount;
       }
     });
   };
 
   return (
-    <button onClick={handleClick} className="count-btn">
+    <button disabled={locked} onClick={handleClick} className="count-btn">
       {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
       ) : (
